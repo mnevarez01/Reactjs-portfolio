14a9c2f4612ed23b2379281c1500a92f4f4475dc
Maria Nevarez <mnevarez1220@gmail.com>
2020-05-26 16:43:38 -0600

    updated server.js

--- src/component/Portfolio/index.js
+++ src/component/Portfolio/index.js
@@ -1,11 +1,19 @@
-import React, { useContext } from 'react';
-import CardContainer from '../CardContainer'
+import React from 'react';
+import pic1 from '../../../public/assets/pic1'
+
 
 function Portfolio() {
   return (
     <>
-      <div>This will be the Portfolio page</div>
-      <CardContainer />
+      <h2>Alewery</h2>
+      <img href='https://mighty-depths-48025.herokuapp.com/' src={pic1} alt='Alewery' />
+      <p>A site that contains all beers from your local breweries! To all breweries have your beers accessible to all!</p>
+      <h2>COVID Quarantainment</h2>
+      <img href="https://mnevarez01.github.io/tmtg/" src={pic2} alt='Covid' />
+      <p>A While facing hard times building a website that is interactive and distracting to the mind.</p>
+      <h2>Alewery</h2>
+      <img href="https://mnevarez01.github.io/APIWeatherInterface/" src={pic3} alt='Weather' />
+      <p>Using the weather API you are able to look for weather for any city and have your searches saved.</p>
     </>
   )
 }
---
+++ src/utils/PortfolioPages.js
@@ -0,0 +1,17 @@
+const PortfolioPages = [
+  {
+    image: '../assets/noteTaker.PNG',
+    title: "Note Taker ",
+    link: "https://sleepy-ocean-58419.herokuapp.com/",
+    description: 'An application using Nodejs to be able to keep track of all your notes and the ones that need to be completed'
+  },
+  {
+    image: '../assets/password.PNG',
+    title: "Note Taker ",
+    link: "https://sleepy-ocean-58419.herokuapp.com/",
+    description: 'An application using Nodejs to be able to keep track of all your notes and the ones that need to be completed'
+  },
+  {
+
+  }
+]
