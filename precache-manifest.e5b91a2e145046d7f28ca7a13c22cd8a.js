ee4b9550c544d2b4851b3a4f064d25f06e71c1f8
Maria Nevarez <mnevarez1220@gmail.com>
2020-05-28 16:41:57 -0600

    Updates

---
+++ precache-manifest.e5b91a2e145046d7f28ca7a13c22cd8a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8623872d4b5cf8be616fd31148ff6a32",
+    "url": "/mnevarez01/reactjs-portfolio/index.html"
+  },
+  {
+    "revision": "2b4bb67df0af03c6bb59",
+    "url": "/mnevarez01/reactjs-portfolio/static/css/main.bda7232e.chunk.css"
+  },
+  {
+    "revision": "1490f2e64eabfd39e1ed",
+    "url": "/mnevarez01/reactjs-portfolio/static/js/2.07a9cc64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mnevarez01/reactjs-portfolio/static/js/2.07a9cc64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4bb67df0af03c6bb59",
+    "url": "/mnevarez01/reactjs-portfolio/static/js/main.67f81dad.chunk.js"
+  },
+  {
+    "revision": "33f9466c7a6ed485f6f5",
+    "url": "/mnevarez01/reactjs-portfolio/static/js/runtime-main.71da12d7.js"
+  },
+  {
+    "revision": "af95909147f49ef8cbb6cc4ecb9a9362",
+    "url": "/mnevarez01/reactjs-portfolio/static/media/background1.af959091.jpg"
+  },
+  {
+    "revision": "7255221561d5c1e6afc1513f5bb2b0c8",
+    "url": "/mnevarez01/reactjs-portfolio/static/media/pic1.72552215.png"
+  },
+  {
+    "revision": "1ebf865cf99457bcff9e3ef141863ed6",
+    "url": "/mnevarez01/reactjs-portfolio/static/media/pic2.1ebf865c.PNG"
+  },
+  {
+    "revision": "b34cb93a3bd592ffae8a8831e7a3d135",
+    "url": "/mnevarez01/reactjs-portfolio/static/media/pic3.b34cb93a.PNG"
+  }
+]);
