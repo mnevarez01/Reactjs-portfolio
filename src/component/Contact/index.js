f9db467c64591ba01f45890fd2f59d877123f8d2
Maria Nevarez <mnevarez1220@gmail.com>
2020-05-27 20:33:16 -0600

    updated contact page

--- src/component/Contact/index.js
+++ src/component/Contact/index.js
@@ -3,31 +3,33 @@ import './style.css'
 
 const Contact = () => {
   return (
-    <div className="ContactPage">
-      <h3>Please reach out!</h3>
+    <>
+      <h2>Contact Me:</h2>
+      <div className="ContactPage">
+        <h3>Please reach out!</h3>
 
-      <form>
-        <div className="form-group">
-          <label for="exampleFormControlInput1">Name</label>
-          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Name" />
-        </div>
-        <hr />
-        <div className="form-group">
-          <label for="exampleFormControlInput1">Email address</label>
-          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-        </div>
-        <hr />
+        <form>
+          <div className="form-group">
+            <label for="exampleFormControlInput1">Name</label>
+            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Name" />
+          </div>
+          <hr />
+          <div className="form-group">
+            <label for="exampleFormControlInput1">Email address</label>
+            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+          </div>
+          <hr />
 
-        <div className="form-group">
-          <label for="exampleFormControlTextarea1">Message</label>
-          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Leave a Message!"></textarea>
-        </div>
-        <button type="submit" className="btn btn-outline-dark Submit">Send</button>
+          <div className="form-group">
+            <label for="exampleFormControlTextarea1">Message</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Leave a Message!"></textarea>
+          </div>
+          <button type="submit" className="btn btn-outline-dark Submit">Send</button>
 
-      </form>
+        </form>
 
-    </div>
-
+      </div>
+    </>
   )
 }
 export default Contact
